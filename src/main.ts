d1cc22b6f21521d90c5b33dc2648afb5d3d44f28
natalalozinskaa <guvegina15N!>
2023-08-22 14:32:55 +0500

    feat(custom): добавить свагер

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { CAppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(CAppModule);
 
-  await app.listen(3000);
+  const configSwagger = new DocumentBuilder()
+    .setTitle('API bank microservice')
+    .setDescription('API для bank microservice')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, configSwagger);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT, () => console.log(`Service started on port ${PORT}`));
 }
 bootstrap();
