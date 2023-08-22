edd7bb4a20ca846c52fabf04c33200921a5d609b
natalalozinskaa <guvegina15N!>
2023-08-22 18:38:42 +0500

    style(account): применить линтер

--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { CAccountService } from './account.service';
+import { CAccountDto } from './dto/account.dto';
 import { CCreateAccountDto } from './dto/create-account.dto';
 import { CUpdateAccountDto } from './dto/update-account.dto';
-import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CAccountDto } from './dto/account.dto';
 
 @ApiTags('Аккаунты')
 @Controller('account')
@@ -12,39 +12,39 @@ export class CAccountController {
   constructor(private readonly accountService: CAccountService) {}
 
   @Post()
-  @ApiOperation({summary: 'Добавление аккаунта'})
-  @ApiBody({type: CCreateAccountDto, required: true, description: 'Описание аккаунта'})
-  @ApiResponse({status: HttpStatus.OK, description: 'Добавленный аккаунт', type: CAccountDto})
+  @ApiOperation({ summary: 'Добавление аккаунта' })
+  @ApiBody({ type: CCreateAccountDto, required: true, description: 'Описание аккаунта' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Добавленный аккаунт', type: CAccountDto })
   create(@Body() createAccountDto: CCreateAccountDto) {
     return this.accountService.create(createAccountDto);
   }
 
   @Get()
-  @ApiOperation({summary: 'Получение всех аккаунтов'})
-  @ApiResponse({status: HttpStatus.OK, description: 'Список аккаунтов', type: [CAccountDto]})
+  @ApiOperation({ summary: 'Получение всех аккаунтов' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Список аккаунтов', type: [CAccountDto] })
   findAll() {
     return this.accountService.findAll();
   }
 
   @Get(':id')
-  @ApiOperation({summary: 'Получение аккаунта по id'})
-  @ApiParam({name: 'id', type: Number, required: true, description: 'ID аккаунта'})
-  @ApiResponse({status: HttpStatus.OK, description: 'Найденный аккаунт', type: CCreateAccountDto})
+  @ApiOperation({ summary: 'Получение аккаунта по id' })
+  @ApiParam({ name: 'id', type: Number, required: true, description: 'ID аккаунта' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Найденный аккаунт', type: CCreateAccountDto })
   findOne(@Param('id') id: number) {
     return this.accountService.findOne(id);
   }
 
   @Patch(':id')
-  @ApiOperation({summary: 'Изменение аккаунта по id'})
-  @ApiParam({name: 'id', type: Number, required: true, description: 'ID аккаунта'})
-  @ApiResponse({status: HttpStatus.OK, description: 'Изменённый аккаунт', type: CAccountDto})
+  @ApiOperation({ summary: 'Изменение аккаунта по id' })
+  @ApiParam({ name: 'id', type: Number, required: true, description: 'ID аккаунта' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Изменённый аккаунт', type: CAccountDto })
   update(@Param('id') id: number, @Body() dto: CUpdateAccountDto) {
     return this.accountService.update(id, dto);
   }
 
   @Delete(':id')
-  @ApiOperation({summary: 'Удаление аккаунта по id'})
-  @ApiParam({name: 'id', type: Number, required: true, description: 'ID аккаунта'})
+  @ApiOperation({ summary: 'Удаление аккаунта по id' })
+  @ApiParam({ name: 'id', type: Number, required: true, description: 'ID аккаунта' })
   remove(@Param('id') id: string) {
     return this.accountService.remove(+id);
   }
