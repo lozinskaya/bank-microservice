9831925890e0ceaf74957e953c10256ed9f2c44e
natalalozinskaa <guvegina15N!>
2023-08-23 00:08:33 +0500

    fix: Установить минимальное значение для баланса

--- src/modules/account/dto/create-account.dto.ts
+++ src/modules/account/dto/create-account.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDefined, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsDefined, IsNotEmpty, IsNumber, Min } from 'class-validator';
 
 export class CCreateAccountDto {
   @ApiProperty({ description: 'Баланс' })
   @IsDefined()
   @IsNotEmpty()
   @IsNumber()
+  @Min(1)
   balance: number;
 }
--- src/modules/transfer/dto/create-transfer.dto.ts
+++ src/modules/transfer/dto/create-transfer.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDefined, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsDefined, IsNotEmpty, IsNumber, Min } from 'class-validator';
 
 export class CCreateTransferDto {
   @ApiProperty({ description: 'Сумма перевода' })
   @IsDefined()
   @IsNotEmpty()
   @IsNumber()
+  @Min(1)
   amount: number;
 }
